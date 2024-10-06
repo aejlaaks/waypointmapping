69fbb666a26ccbd2caccbb2b8b0355d1f12afa5f
Antti Laakso <antti.laakso@careeria.fi>
2024-10-06 09:56:33 +0300

    fix for apibaseurl

--- reactapp2.client/src/services/api.js
+++ reactapp2.client/src/services/api.js
@@ -1,6 +1,5 @@
 import axios from 'axios'; 
-const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;;
-// Luodaan axios-instanssi, jossa on perus URL-osoit
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL || '';// Luodaan axios-instanssi, jossa on perus URL-osoit
 
 const api = axios.create({
   baseURL: `${apiBaseUrl}/api/`,  // Oikea tapa käyttää template stringiä
